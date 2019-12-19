ae9625e8a86ebd55017023ad6ca614167943a97f
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-19 20:48:15 +1300

    Create http path in app.js. Create (hard-code) posts[] and pass 'posts' into express's 'json()' body parser. Run/Test path in browser if posts[] is parsed.

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,26 @@ const express = require('express'); // import 'express'
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Hello from express!!!');
+app.use('/api/posts', (req, res, next) => {
+
+  const posts = [
+    {
+      id: 'qwer123',
+      title: 'First server-side TITLE',
+      content: 'Content #1 from the server!'
+    },
+    {
+      id: 'poiu123',
+      title: 'Second server-side TITLE',
+      content: 'Content #2 from the server!'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
+
 });
 
 
