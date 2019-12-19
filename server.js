9da79f01ec150a639f7dd1966e1e2999bb5659af
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-19 20:21:39 +1300

    Install nodemon to watch project changes and automatic-update. Improve server.js adding normalizePort and onError for error handling.Import 'debug' for nodemon to work.

--- server.js
+++ server.js
@@ -1,9 +1,67 @@
-const http = require('http'); // Node module/package import
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
+
+
+const normalizePort = val => {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+
+//  ERROR Handler
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
+
+
+
+/*const http = require('http'); // Node module/package import
 const app = require('./backend/app'); // IMPORT the app/js
 
-/*const server = http.createServer((req, res) => {
+/!*const server = http.createServer((req, res) => {
   res.end('<h1>This is my first response</h1>');
-});*/
+});*!/
 
 const port = process.env.PORT || 3000;
 
@@ -11,7 +69,7 @@ app.set('port', port);
 const server = http.createServer(app);
 
 
-/* Set-up Node to listen to var PORT...
+/!* Set-up Node to listen to var PORT...
   * if PORT is not set, use port: 3000
-  */
-server.listen(port);
+  *!/
+server.listen(port);*/
